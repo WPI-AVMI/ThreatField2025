f2b739a5a3df3e39b62979dceebc770b0aa6b77a
eldavo07 <delias.intr@gmail.com>
2025-07-21 15:19:41 -0400

    launch file added

---
+++ workspace/ros2_ws/build/interface/ament_cmake_python/interface/interface/msg/__init__.py
@@ -0,0 +1,1 @@
+from interface.msg._sensor import Sensor  # noqa: F401
--- workspace/ros2_ws/src/py_sensor/launch/sense_launch.py
+++ workspace/ros2_ws/src/py_sensor/launch/sense_launch.py
@@ -2,12 +2,18 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
+import os
 
 def generate_launch_description():
-
     micro_ros_agent = ExecuteProcess(
         cmd=[[
-            'micro-ros-agent udp4 --port 8888 -v '
+            'MicroXRCEAgent udp4 --port 8888 -v '
+        ]],
+        shell=True
+    )
+    clock_translation_node = ExecuteProcess(
+        cmd=[[
+            'ros2 run ros_gz_bridge parameter_bridge /clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'
         ]],
         shell=True
     )
@@ -19,7 +25,18 @@ def generate_launch_description():
         shell=True,
     )
 
+    QGroundcontrol_node = ExecuteProcess(
+        cmd=[[
+            '/home/avmi-lab-user/Downloads/QGroundControl-x86_64.AppImage'
+            
+        ]],
+        output='screen',
+        shell=True
+    )
+
     return LaunchDescription([
-        #micro_ros_agent,
-        sense_node
+        micro_ros_agent,
+        sense_node,
+        QGroundcontrol_node,
+        clock_translation_node
     ])
